5b1350dd4f03dc1fa6c0ff1e6bad93541106a785
Marcelo7Bastos <marceloc.bastos@hotmail.com>
2023-03-03 18:03:23 -0300

    Refactor code to improve readability and add clickable links to project descriptions. Update project information, including new links for project 2 and 3.

---
+++ project_data_science.js
@@ -0,0 +1,23 @@
+var projetos = {
+  "projeto1": {
+    "titulo": "Projetos em Ciência de Dados",
+    "descricao": "Painel de Monitoramento COVID-19 (2022)",
+    "ferramenta": "Power BI",
+    "imagem": "images/project_power_bi_2021_painel_covid.jpg",
+    "link": "https://app.powerbi.com/view?r=eyJrIjoiN2I0NWY1Y2QtNTE4OS00MTZjLTk0ZjQtYTk4ODM4MTM4NTNlIiwidCI6IjNlYzkyOTY5LTVhNTEtNGYxOC04YWM5LWVmOThmYmFmYTk3OCJ9"
+  },
+  "projeto2": {
+    "titulo": "Word Cloud - OECD Guidlines (2020)",
+    "descricao": "Avaliar a evolução na linguagem utilizada entre os anos 2000 e 2018 em textos sobre as Políticas de Conduta Empresarial.",
+    "ferramenta": "Python, Jupyter Notebook",
+    "imagem": "images/world_cloud_project.jpg",
+    "link": "..\projects\2020_TrabalhoFinal_CienciaDado_Enap_2020.html"
+  },
+  "projeto3": {
+    "titulo": "Projetos de Análise de Dados em Linguagem R - Curso Enap",
+    "descricao": "Recentemente concluí o curso 'Análise de Dados em Linguagem R' da Enap e fui desafiado a replicar dois projetos apresentados ao final do curso. No primeiro projeto, utilizei o RMarkdown e alguns pacotes para limpeza e transformação dos dados, além de propor um algoritmo para automatizar o download das bases. O resultado pode ser acessado em: <a href='https://marcelo7bastos.github.io/analise-dados-r-enap-viagens/' target='_blank'>https://marcelo7bastos.github.io/analise-dados-r-enap-viagens/</a>. Já no segundo projeto, desenvolvi um dashboard utilizando o Shiny. O resultado pode ser acessado em: <a href='https://rh1r2a-marcelo0bastos.shinyapps.io/analise_dados_diabetes_enap_publicacao/' target='_blank'>https://rh1r2a-marcelo0bastos.shinyapps.io/analise_dados_diabetes_enap_publicacao/</a>. Agradeço à Enap pelas oportunidades de aprendizado. Sobre PDI, fiz um post aqui no linkedin: <a href='https://www.linkedin.com/pulse/quer-crescer-na-carreira-fa%C3%A7a-seu-pdi-marcelo-bastos/?trk=public_post_embed_feed-article-content' target='_blank'>https://www.linkedin.com/pulse/quer-crescer-na-carreira-fa%C3%A7a-seu-pdi-marcelo-bastos/?trk=public_post_embed_feed-article-content</a>.",
+    "ferramenta": "R, RStudio, RMarkdown, Shiny",
+    "imagem": "images/analise-dados-inguagem-r-curso-enap.jpg",
+    "link": "https://www.linkedin.com/posts/marcelo-cabreira-bastos_pdi-aprendizagem-cienciadedados-activity-7036912312930181120-nUko?utm_source=share&utm_medium=member_desktop"
+  }
+};
